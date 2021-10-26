74decab60443b3fcefabbfaca5c03514794edebb
Richmond Martey <marteyrichmond001@gmail.com>
2021-10-26 14:50:01 +0000

    added screens

---
+++ screens/cart.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+
+const cart = () => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Cart</Text>
+      </View>
+    );
+  }
+
+  export default cart;
---
+++ screens/login.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+
+const logIn = () => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>LogIn</Text>
+      </View>
+    );
+  }
+
+  export default logIn;
